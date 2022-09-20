215bad8cf56fcff2d6802b040a7a5015c96e0b02
Rlucas000 <112975622+Rlucas000@users.noreply.github.com>
2022-09-20 09:08:00 +0200

    Add files via upload

---
+++ algo/tp1/algo_python.py
@@ -0,0 +1,39 @@
+
+"""
+a = 4
+b = 3.2
+c = "hello"
+print(a,b,c,"->",end="")  #pas de retour à la ligne
+print(a+5)  #affichage de texte avec retour à la ligne
+print("bonjour")  #affichage de texte avec retour à laligne
+
+a = 20
+if a>10:
+    print("supérieur")
+else:print("inférieur")
+
+a=34
+b=6
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a//b
+h=a%b
+i=b**2
+print(c,d,e,f,g,h,i)
+
+
+A = 3
+b = 15
+r = A + b
+"""
+A = 3
+b = 15
+r = A + b
+
+if (r>0) :
+    print("positif")
+else (r<0) :
+    print("négatif")
+
